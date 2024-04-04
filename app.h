5708c02b7ce2bca78dd9151f2e00434e9ee76fde
ernest <ernestbardon@gmail.com>
2024-04-04 21:20:12 +0200

    set up imgui
    Signed-off-by: ernest <ernestbardon@gmail.com>

---
+++ app.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
+
+namespace app {
+    void RenderUI();
+//    void LoadTexture(const char* filename, GLuint* texture);
+}
